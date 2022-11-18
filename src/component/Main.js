0c645c0eca9ff92502a9ef123bed1f7b942b33cd
choho4843 <choho4843@gmail.com>
2022-11-18 15:48:38 +0900

    Section1 add

--- src/component/Main.js
+++ src/component/Main.js
@@ -1,20 +1,25 @@
 import { Component } from "react";
-import {Table} from 'reactstrap';
+import { Table } from 'reactstrap';
+import Section1 from "./Section1";
 import Section0 from "./Section0";
+import Section2 from "./Section2";
+
 
 class Main extends Component {
-    render(){
-        return(
-        <div className='main'>
-            <Table borderless size='sm'>
-                <tbody>
-                    <Section0/>
-                </tbody>
-            </Table>
+    render() {
+        return (
+            <div className='main'>
+                <Table borderless size='sm'>
+                    <tbody>
+                        <Section0 />
+                        <Section1 />
+                        <Section2 />
+
+                    </tbody>
+                </Table>
             </div>
         )
     }
 }
-  
-  export default Main;
-  
+
+export default Main;
