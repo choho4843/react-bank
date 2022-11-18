fa6253ce450cb600716251724f9085ac24f2acb4
choho4843 <choho4843@gmail.com>
2022-11-18 17:07:50 +0900

    Section5 add

--- src/component/Main.js
+++ src/component/Main.js
@@ -4,6 +4,8 @@ import Section1 from "./Section1";
 import Section0 from "./Section0";
 import Section2 from "./Section2";
 import Section3 from "./Section3";
+import Section4 from "./Section4";
+import Section5 from "./Section5";
 
 
 class Main extends Component {
@@ -16,7 +18,8 @@ class Main extends Component {
                         <Section1 />
                         <Section2 />
                         <Section3 />
-
+                        <Section4 />
+                        <Section5 />
                     </tbody>
                 </Table>
             </div>
--- src/component/Section3.js
+++ src/component/Section3.js
@@ -1,29 +1,30 @@
 import { Component } from "react";
-import {Button, Badge} from "reactstrap"
+import { Button, Badge } from "reactstrap"
 
-class Section3 extends Component{
-    render(){
-        return(
+class Section3 extends Component {
+    render() {
+        return (
             <tr>
                 <td className={"w-50"}>
                     <div className="imgBox">
                         <img src={require('../img/main-moim.png')}
-                        height='750px' width='550px' alt=''/>
+                            height='750px' width='550px' alt='' />
                     </div>
                 </td>
                 <td className={"w-50"}>
                     <div className="textBox">
                         <span className="l-text">
-                            함께 쓰고 같이 보는 <br/>
+                            함께 쓰고 같이 보는 <br />
                             <Badge color='warning'>B</Badge>모임통장
-                        </span><br/>
+                        </span><br />
                         <span className="s-text">
-                            토토톡 친구들 바로 모임통장으로 초대하고<br/>
-                            친구들과 함께 잔액과 입출금 현황을 확인할 수 있습니다.<br/>
+                            토토톡 친구들 바로 모임통장으로 초대하고<br />
+                            친구들과 함께 잔액과 입출금 현황을 확인할 수 있습니다.<br />
                             재미있는 메시지 카드로 회비 입금 요청도 해보세요.
-                        </span><br/>
+                        </span><br />
+                        <Button color='light' size='sm'>모임통장</Button>
                     </div>
-                    <Button color='light' size='sm'>모임통장</Button>
+
                 </td>
             </tr>
         )
